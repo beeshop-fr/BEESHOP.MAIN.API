2f0664692cec0688984cbd59e98488d2412ad54a
Louis <louis.perrin@diiage.org>
2025-08-25 12:44:58 +0200

    fix cors

--- BEESHOP.MAIN.API/Program.cs
+++ BEESHOP.MAIN.API/Program.cs
@@ -60,8 +60,9 @@ catch (Exception ex)
     Console.WriteLine($"Erreur lors du test de connexion PostgreSQL : {ex.Message}");
     throw;
 }
-//app.UseCors();
+
 app.UseStaticFiles();
+app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();
 
